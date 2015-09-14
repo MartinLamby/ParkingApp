ab1bb7e028af8539703b137713d40a48382cb0a1
martinlamby <martinlamby@live.de>
2015-09-14 18:12:36 +0200

    Location and Shake Event Added

---
+++ app/src/main/java/com/example/martinlamby/parking/GeoLocationService.java
@@ -0,0 +1,89 @@
+package com.example.martinlamby.parking;
+
+import android.app.Service;
+import android.content.Intent;
+import android.location.Location;
+import android.os.Bundle;
+import android.os.IBinder;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
+
+public class GeoLocationService extends Service implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener,LocationListener {
+
+    private static final int TIME_INTERVAL = 1000;
+    private static GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
+    private static Location lastLocation;
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public void onCreate() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+
+        mGoogleApiClient.connect();
+
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        System.out.println("GeoLocationService started");
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        System.out.println("GeoLocationService stopped");
+        super.onDestroy();
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        System.out.println("NEW LOCATION    " + location.getLatitude());
+        lastLocation = location;
+    }
+
+    public static double getLastLocationLatitude(){
+        lastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        return lastLocation.getLatitude();
+    }
+    public static double getLastLocationLongitude(){
+        lastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        return lastLocation.getLongitude();
+    }
+
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        mLocationRequest = LocationRequest.create();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(TIME_INTERVAL);
+        mLocationRequest.setFastestInterval(TIME_INTERVAL/2);
+
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient,mLocationRequest, this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        System.out.println("Connection Suspended   "+i);
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        System.out.println("Connection Failed----ErrorResult:"+connectionResult.getErrorCode());
+    }
+}
---
+++ app/src/main/java/com/example/martinlamby/parking/ShakeDetectorService.java
@@ -0,0 +1,129 @@
+package com.example.martinlamby.parking;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.CountDownTimer;
+import android.os.IBinder;
+import android.widget.Toast;
+
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+public class ShakeDetectorService extends Service {
+
+    private static final int SHAKE_INTERVAL = 1000;
+    private CountDownTimer shakeIntervalSeperaterTimer;
+    private boolean shakeAlreadyHappened;
+    private SensorManager mSensorManager;
+    private float mAccel;
+    private float mAccelCurrent;
+    private float mAccelLast;
+
+    private final SensorEventListener mSensorListener = new SensorEventListener() {
+
+        public void onSensorChanged(SensorEvent event) {
+            float xAcc = event.values[0];
+            float yAcc = event.values[1];
+            float zAcc = event.values[2];
+            mAccelLast = mAccelCurrent;
+            mAccelCurrent = (float) Math.sqrt((double) (xAcc * xAcc + yAcc * yAcc + zAcc * zAcc));
+            float delta = mAccelCurrent - mAccelLast;
+            mAccel = mAccel * (float) 0.9 + delta;
+
+            shakeSeperationTimer();
+
+            if (shakeAlreadyHappened == false && mAccel > 12) {
+                    onShakeEvent();
+            }
+
+        }
+
+        public void onShakeEvent(){
+            shakeIntervalSeperaterTimer.start();
+            shakeAlreadyHappened = true;
+
+            //get Location (will be replaced with Data from GeoLocationService (not existend to this date 13.09.2015)
+            System.out.println("SHAKE DETECTED");
+
+            saveParkedCarPositionToParse(GeoLocationService.getLastLocationLatitude(),GeoLocationService.getLastLocationLongitude());
+            Toast toast = Toast.makeText(getApplicationContext(), "Position saved", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+
+        public void shakeSeperationTimer(){
+            shakeIntervalSeperaterTimer = new CountDownTimer(SHAKE_INTERVAL, SHAKE_INTERVAL) {
+                @Override
+                public void onTick(long millisUntilFinished) {
+                    //not needed
+                }
+
+                @Override
+                public void onFinish() {
+                    System.out.println("Timer finished");
+                    shakeAlreadyHappened = false;
+                }
+            };
+        }
+
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+            //not needed
+        }
+
+
+    };
+
+
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mSensorManager.registerListener(mSensorListener, mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_UI);
+        mAccel = (float) 0.00;
+        mAccelCurrent = SensorManager.GRAVITY_EARTH;
+        mAccelLast = SensorManager.GRAVITY_EARTH;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        System.out.println("ShakeDetectorService started");
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        System.out.println("ShakeDetectorService stopped");
+        super.onDestroy();
+    }
+
+    public void saveParkedCarPositionToParse(double latitude, double longitude) {
+        ParseObject parkedCarPosition = new ParseObject("ParkedCarPosition");
+        parkedCarPosition.add("latitude", latitude);
+        parkedCarPosition.add("longitude", longitude);
+        parkedCarPosition.add("username", ParseUser.getCurrentUser().getUsername());
+        parkedCarPosition.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null) {
+                    //System.out.println("Parked Car Position has been saved successfully");
+                } else {
+                    System.out.println("Failed to saved Position of parked Car");
+                }
+            }
+        });
+    }
+
+}
